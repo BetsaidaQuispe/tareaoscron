f8368bf39cacbfd69ad63c647bf5b9c1400ca395
BetsaidaQuispe <betsaida.aquino@gmail.com>
2017-05-15 20:04:01 -0500

    mis commits

--- subir.sh
+++ subir.sh
@@ -6,6 +6,7 @@ git commit -a -m "mis commits"
 git push -u origin master 
 
 echo "segundo commit de prueba  con el proceso cron -- lunes "
+echo "segundo commit de prueba  con el proceso cron -- lunes 8:00pm "
 
 
 
